709910806548c42444412de987e5cce581241917
oqpvc <o+github@qp.vc>
2017-04-15 11:52:30 +0200

    fck autogenerated tests

--- test/controllers/examples_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ExampleControllerTest < ActionDispatch::IntegrationTest
-  test "should get view" do
-    get example_view_url
-    assert_response :success
-  end
-
-end
