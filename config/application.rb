ccb23b8b6f3b2f76cdce74106dcf1a7485819250
oqpvc <o+github@qp.vc>
2017-04-26 14:41:06 +0200

    fix localization fallback issues

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Erdbeere
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir[File.join(Rails.root, "lib", "**", "*.rb")].each {|l| require l }
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
     config.i18n.fallbacks = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'examples#list'
+  root to: redirect('/de')
   scope '/:locale', locale: /#{I18n.available_locales.join('|')}/ do
     resources :examples
     post '/examples/find'
